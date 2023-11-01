be48b2882dd3915d9a7f52548b771722ee6e44b1
SuayipEmre <suayipemredev@gmail.com>
2023-11-01 16:11:31 +0300

    some bugs fixed

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { GenreList } from '../genres'
 import { Link } from 'react-router-dom'
 import { Search } from './search'
+import { setSearchTitle } from '~/redux/features/search/actions'
 
 
 export const Header = () => {
@@ -10,7 +11,7 @@ export const Header = () => {
   
      <div className=' block  md:flex md:justify-between'>
           <Link to={'/'}   className=''>
-                <h3 className='text-red-600'>MovieApp</h3>
+                <h3 onClick={() =>  setSearchTitle('')} className='text-red-600'>MovieApp</h3>
          </Link>
       <GenreList />
      </div>
--- src/components/movieCart/index.jsx
+++ src/components/movieCart/index.jsx
@@ -3,44 +3,44 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { setSearchTitle } from '~/redux/features/search/actions'
 
-export const MovieCart = ({movie}) => {
+export const MovieCart = ({ movie }) => {
     const navigate = useNavigate()
-    
+
     const handleClick = () => {
         navigate(`/detail/${movie.id}`)
         setSearchTitle('')
     }
     return (
-        <div 
-        onClick={handleClick }
-        className='col-span-12  sm:col-span-6 lg:col-span-4 xl:col-span-3  relative cursor-pointer  '>
-           
-           <h3 className='absolute top-0 inset-x-8 bg-black/20 rounded-sm text-center  '>
-           {
-                movie.title
-            }
-    
-           
-           </h3>
-    
-           <div>
+        <div
+            onClick={handleClick}
+            className='col-span-12  sm:col-span-6 lg:col-span-4 xl:col-span-3  relative cursor-pointer  '>
+
+            <h3 className='absolute top-0 inset-x-8 bg-black/20 rounded-sm text-center  '>
+                {
+                    movie.title
+                }
+
+
+            </h3>
+
+            <div>
                 <img src={`${import.meta.env.VITE_API_IMAGE_PATH}${movie.backdrop_path}`} alt="" />
             </div>
-            <div className={classNames('absolute right-2   text-black px-2 rounded-full bottom-2',{
-    
-         "bg-avgColor-default" : Math.round(movie?.vote_average) > 8.5,
-         "bg-avgColor-orange" : Math.round(movie?.vote_average) < 8.5,
-         "bg-avgColor-red" : Math.round(movie?.vote_average) < 5,
+            <div className={classNames('absolute right-2   text-black px-2 rounded-full bottom-2', {
+
+                "bg-avgColor-default": Math.round(movie?.vote_average) > 8.5,
+                "bg-avgColor-orange": Math.round(movie?.vote_average) < 8.5,
+                "bg-avgColor-red": Math.round(movie?.vote_average) < 5,
             })}>
                 <p>
-                {
-                    Math.round(movie?.vote_average) 
-                
-               }
+                    {
+                        Math.round(movie?.vote_average)
+
+                    }
                 </p>
-    
-    
+
+
             </div>
         </div>
-      )
+    )
 }
